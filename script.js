52157951a0b8e4eb5a2dad801942fb5539a1bab8
fiba fathima <fibaaminnu@gmail.com>
2025-03-20 14:49:26 +0530

    completed assignment

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+const jwt = require('jsonwebtoken')
+
+const secretKey = '123abc456'
+
+
+
+
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  return jwt.sign(payload,secretKey,{expiresIn : '1h'})
 }
 
 const decrypt = (token) => {
+  try {
+    return jwt.verify(token,secretKey)
+    
+  } catch (error) {
+    return {error:'Invalid or expired token'}
+    
+  }
   // return decoded payload
+}
+
+const payload = { userId: 123, role: 'admin' };
+const token = encrypt(payload);
+const decoded = decrypt(token);
+
+if (decoded.userId === payload.userId && decoded.role === payload.role) {
+  console.log('Success');
+} else {
+  console.log('Failed');
 }
 
 module.exports = {
